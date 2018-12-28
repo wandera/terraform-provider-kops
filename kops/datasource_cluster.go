b25c41ba68b1ba9714a50ead0a70de9cb6074eab
jakub.coufal <jakub.coufal@wandera.com>
2018-12-28 21:24:18 +0100

    Update datasource

--- kops/datasource_cluster.go
+++ kops/datasource_cluster.go
@@ -11,21 +11,8 @@ func dataSourceCluster() *schema.Resource {
 			State: schema.ImportStatePassthrough,
 		},
 		Schema: map[string]*schema.Schema{
-			"state_store": {
-				Type:        schema.TypeString,
-				Required:    true,
-				DefaultFunc: schema.EnvDefaultFunc("KOPS_STATE_STORE", nil),
-				Description: descriptions["state_store"],
-			},
-			"name": {
-				Type:     schema.TypeString,
-				Computed: true,
-				Optional: true,
-			},
-			"content": {
-				Type:     schema.TypeString,
-				Computed: true,
-			},
+			"metadata": schemaMetadata(),
+			"spec":     schemaClusterSpec(),
 		},
 	}
 }
--- kops/provider.go
+++ kops/provider.go
@@ -27,11 +27,11 @@ func Provider() terraform.ResourceProvider {
 			},
 		},
 		DataSourcesMap: map[string]*schema.Resource{
-			"kops_aws_cluster":        dataSourceCluster(),
-			"kops_aws_instance_group": dataSourceInstanceGroup(),
+			"kops_cluster":        dataSourceCluster(),
+			"kops_instance_group": dataSourceInstanceGroup(),
 		},
 		ResourcesMap: map[string]*schema.Resource{
-			"kops_aws_cluster": resourceCluster(),
+			"kops_cluster": resourceCluster(),
 			//"kops_aws_instance_group": resourceInstanceGroup(),
 		},
 		ConfigureFunc: configureProvider,
@@ -63,22 +63,5 @@ var descriptions map[string]string
 func init() {
 	descriptions = map[string]string{
 		"state_store": "Location of state storage.",
-		"region": "The region where AWS operations will take place. Examples\n" +
-			"are us-east-1, us-west-2, etc.",
-
-		"access_key": "The access key for API operations. You can retrieve this\n" +
-			"from the 'Security & Credentials' section of the AWS console.",
-
-		"secret_key": "The secret key for API operations. You can retrieve this\n" +
-			"from the 'Security & Credentials' section of the AWS console.",
-
-		"profile": "The profile for API operations. If not set, the default profile\n" +
-			"created with `aws configure` will be used.",
-
-		"shared_credentials_file": "The path to the shared credentials file. If not set\n" +
-			"this defaults to ~/.aws/credentials.",
-
-		"token": "session token. A session token is only required if you are\n" +
-			"using temporary security credentials.",
 	}
 }
